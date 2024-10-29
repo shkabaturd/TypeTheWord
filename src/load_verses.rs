bf16daf2ccef1ab891ceb7db94c1fb921a231927
Denis Shkabatur <shkabaturd@yandex.ru>
2024-10-29 14:56:01 +0300

    add field pickers, add next button

--- src/load_verses.rs
+++ src/load_verses.rs
@@ -7,15 +7,15 @@ use std::{collections::HashMap, include_str};
 pub struct Verse {
     pub book_id: String,
     pub book_name: String,
-    pub chapter: u32,
-    pub verse: u32,
+    pub chapter: usize,
+    pub verse: usize,
     pub text: String,
 }
 
 #[derive(Debug, Default, Clone)]
 pub struct Book {
     pub book_name: String,
-    pub chapters: HashMap<u32, Vec<Verse>>,
+    pub chapters: HashMap<usize, Vec<Verse>>,
 }
 
 const VERSES_TEXT_RST: &str = include_str!("../bible_translations/rst.json");
@@ -33,7 +33,7 @@ pub fn load_rst_verses() -> Vec<Verse> {
 
 pub fn get_book(book_name: String, verses: Vec<Verse>) -> Book {
     let all_book_verses: Vec<Verse> = verses.into_iter().filter(|v| v.book_name == book_name).collect();
-    let mut chapters: HashMap<u32, Vec<Verse>> = HashMap::new();
+    let mut chapters: HashMap<usize, Vec<Verse>> = HashMap::new();
 
     for verse in all_book_verses {
         chapters.entry(verse.chapter).or_insert(Vec::new()).push(verse);
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,16 @@ use std::default;
 
 use load_verses::{load_rst_verses, get_book, Verse, Book};
 use iced::{Element, Task,};
-use iced::widget::{pick_list, column, row, text,};
+use iced::widget::{pick_list, column, row, text, button};
 
 
 #[derive(Default)]
 struct AppState {
+    all_verses: Vec<Verse>,
     current_book: Book,
     book_names: Vec<String>,
-    current_chapter: u32,
-    current_verse: u32,
+    current_chapter: usize,
+    current_verse: usize,
     current_verse_text: String,
     user_input: String,
 }
@@ -19,16 +20,18 @@ struct AppState {
 #[derive(Debug, Clone)]
 enum Message {
     BookPiked(String),
-    ChapterPicked(u32),
-    VersePicked(u32),
+    ChapterPicked(usize),
+    VersePicked(usize),
+    NextVerse,
 }
 
 impl AppState {
 
     fn new() -> (Self, Task<Message>) {
         let all_verses = load_rst_verses();
-        let current_book =  get_book("Matthew".to_string(), all_verses);
+        let current_book =  get_book("Matthew".to_string(), all_verses.clone());
        (AppState {
+        all_verses: all_verses,
         book_names: vec!["Matthew".to_string(),"Luke".to_string()],
         current_book: current_book.clone(),
         current_chapter: 1,
@@ -40,16 +43,77 @@ impl AppState {
 
     fn update(&mut self, message: Message) {
 
-        // TODO: добавить subscription для клавиатуры
+        match message {
+            Message::BookPiked(book_name) => {
+                self.current_book = get_book(book_name, self.all_verses.clone());
+                self.current_chapter = 1;
+                self.update_text_verse(1);
+            }
+
+            Message::ChapterPicked(chapter) => {
+                self.current_chapter = chapter;
+                self.update_text_verse(1);
+            }
+
+            Message::VersePicked(verse) => {
+                self.current_verse = verse;
+                self.update_text_verse(verse);
+            }
+
+            Message::NextVerse => {
+                self.next_verse();
+            }
+        }
 
     }
 
-    fn view(&self) -> Element<Message> {
-        let text = text(&self.current_verse_text);
-        println!("{}", self.current_verse_text);
-        let select_book = pick_list(self.book_names.clone(), Some("Matthew".to_string()), Message::BookPiked);
+    fn update_text_verse(&mut self, verse: usize) {
+        self.current_verse_text = self.current_book.chapters.get(&self.current_chapter).unwrap()[verse-1].text.clone();
+        self.current_verse = verse;
+    }
 
-        column![text, select_book].spacing(10).into()
+    fn next_verse(&mut self) {
+        let verses_in_chapter = self.current_book.chapters.get(&self.current_chapter).unwrap().len();
+        if self.current_verse < verses_in_chapter {
+            self.current_verse += 1;
+            self.update_text_verse(self.current_verse);
+        } else {
+            let chapters_in_book = self.current_book.chapters.keys().len();
+            if self.current_chapter < chapters_in_book {
+                self.current_chapter += 1;
+                self.current_verse = 1;
+                self.update_text_verse(self.current_verse);
+            } else {
+                self.current_chapter = 1;
+                self.current_verse = 1;
+                self.update_text_verse(self.current_verse);
+            }
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+  
+        let book_names = self.book_names.clone();
+        let book_picker = pick_list(book_names, Some(self.current_book.book_name.clone()), Message::BookPiked);
+        
+        let mut book_chapters: Vec<usize> = self.current_book.chapters.clone().into_keys().collect();
+        book_chapters.sort();
+        let chapter_picker: pick_list::PickList<'_, usize, Vec<usize>, usize, Message> = pick_list(book_chapters, Some(self.current_chapter), Message::ChapterPicked);
+
+        let mut chapter_verses: Vec<usize> = self.current_book.chapters.get(&self.current_chapter).unwrap().iter().map(|v| v.verse).collect();
+        chapter_verses.sort();
+        let verse_picker: pick_list::PickList<'_, usize, Vec<usize>, usize, Message> = pick_list(chapter_verses, Some(self.current_verse), Message::VersePicked);
+
+        let current_verse_text = text(&self.current_verse_text);
+
+        let next_button = button("Next verse").on_press(Message::NextVerse);
+
+        let pickers = row![book_picker, chapter_picker, verse_picker, next_button];
+
+        
+        
+        
+        column![pickers, current_verse_text].spacing(10).into()
     }
 }
 fn main() {
